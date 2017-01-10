c3424235086f028c08430fa69f73fc4bd5ecf054
Raghu Betina <raghu@firstdraft.com>
2017-01-10 18:00:02 +0000

    Direct association between positions and seats

--- app/models/position.rb
+++ app/models/position.rb
@@ -1,6 +1,10 @@
 class Position < ApplicationRecord
   # Direct associations
 
+  has_many   :seats,
+             :foreign_key => "official_id",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
--- app/models/seat.rb
+++ app/models/seat.rb
@@ -1,6 +1,9 @@
 class Seat < ApplicationRecord
   # Direct associations
 
+  belongs_to :official,
+             :class_name => "Position"
+
   # Indirect associations
 
   # Validations
