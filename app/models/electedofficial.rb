4a436107262d599eaa62b530a40ee0ec6124e259
Raghu Betina <raghu@firstdraft.com>
2017-01-10 18:00:02 +0000

    Direct association between electedofficials and seats

--- app/models/electedofficial.rb
+++ app/models/electedofficial.rb
@@ -1,6 +1,10 @@
 class Electedofficial < ApplicationRecord
   # Direct associations
 
+  has_many   :seats,
+             :foreign_key => "position_id",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
--- app/models/seat.rb
+++ app/models/seat.rb
@@ -1,6 +1,9 @@
 class Seat < ApplicationRecord
   # Direct associations
 
+  belongs_to :position,
+             :class_name => "Electedofficial"
+
   belongs_to :official,
              :class_name => "Position"
 
